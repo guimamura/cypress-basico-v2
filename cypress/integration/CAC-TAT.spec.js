a40560bc67d84348f31a326c6adca055ce1e091e
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-10-22 12:21:19 -0300

    test: verifying required fields

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,25 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('verifica o título da aplicação', function () {
+    beforeEach(() => {
         cy.visit('./src/index.html')
-        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('verifica o título da aplicação', function () {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function () {
+        cy.get('#firstName').type('John')
+
+        cy.get('#lastName').type('Doe')
+
+        cy.get('#email').type('john_doe@test.com')
+
+        cy.get('#open-text-area').type('Lorem ipsum')
+
+        cy.get('.button').click()
+
+        cy.get('.success').should('be.visible')
     })
 })
