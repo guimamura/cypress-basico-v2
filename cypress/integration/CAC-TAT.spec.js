bc2bd1df9e274e018b430d7c7957122327cdf557
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-10-22 12:30:18 -0300

    test: type text with no delay

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -13,7 +13,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#firstName').type('John')
         cy.get('#lastName').type('Doe')
         cy.get('#email').type('john_doe@test.com')
-        cy.get('#open-text-area').type('Lorem ipsum')
+        cy.get('#open-text-area').type("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", { delay: 0 })
 
         cy.get('button[type="submit"]').click()
 
