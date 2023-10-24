7942b52bbc57d69b3955687760257e46684ccd38
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-10-24 17:15:59 -0300

    test: refac added 'contains' to submit button

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -17,7 +17,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#email').type('john_doe@test.com')
         cy.get('#open-text-area').type(longText, { delay: 0 })
 
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
@@ -28,7 +28,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#email').type('john_doe@test,com')
         cy.get('#open-text-area').type('Teste')
 
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -46,7 +46,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('Teste')
 
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -75,7 +75,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
