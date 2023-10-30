44f70befc28abffa089d4a6ec306845000122cc4
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-10-30 05:40:59 -0300

    test: product selection by value

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -91,4 +91,10 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .select('YouTube')
             .should('have.value', 'youtube')
     })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
 })
