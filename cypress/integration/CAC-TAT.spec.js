21e6fac49afd6080a7ea477058eb13bca139578c
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-10-30 05:55:17 -0300

    test: each radio input option

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -109,4 +109,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .check()
             .should('have.value', 'feedback')
     })
+
+    it('marca cada tipo de atendimento', function () {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function ($radio) {
+                cy.wrap($radio)
+                    .check()
+                    .should('be.checked')
+            })
+    })
 })
