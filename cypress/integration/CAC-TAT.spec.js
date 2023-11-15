083669a5e5d871732c538be45eadf975389c9d5e
guimamura <guilherme.imamura.fernandes@gmail.com>
2023-11-15 20:59:00 -0300

    test: hide cat challenge

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -204,4 +204,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
                 expect(body).to.include('CAC TAT')
             })
     })
+
+    it.only('encontra o gato escondido', function () {
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+        cy.get('#subtitle')
+            .invoke('text', 'Eu 💚 gatos')
+    })
 })
